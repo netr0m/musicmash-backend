b55fbbc65c8e9e7ee9795fc53d100d187bfb92d8
mortea15 <mortea15@student.uia.no>
2018-07-19 22:14:08 +0200

    search across both soundcloud and spotify

---
+++ routes/SearchRoute.js
@@ -0,0 +1,111 @@
+// SearchRoute.js
+
+const express = require('express');
+const request = require('request');
+const buildUrl = require('build-url');
+
+var client_id = process.env.SOUNDCLOUD_ID;
+if (!client_id) { console.log('Missing CLIENT_ID for Soundcloud') }
+const sc = 'soundcloud';
+const sptfy = 'spotify';
+const base = 'http://localhost:4000'
+
+var tracks = {};
+
+const app = express();
+const router = express.Router();
+
+router.route('/:query').get(async (req, res) => {
+    var q = req.params.query.replace(/ /g, '%20');
+    var scEndpoint = sc + '/' + q;
+    var sptfyEndpoint = sptfy + '/' + q;
+
+    scUrl = buildUrl(base, {
+        path: scEndpoint
+    });
+    sptfyUrl = buildUrl(base, {
+        path: sptfyEndpoint
+    });
+
+    var scOptions = {
+        url: scUrl,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+    var sptfyOptions = {
+        url: sptfyUrl,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    function getTracks() {
+        return new Promise((resolve, reject) => {
+            request(scOptions, (e, r, b) => {
+                if (!e) {
+                    results = JSON.parse(b);
+                    tracks['soundcloud'] = results;
+                } else {
+                    console.log(e);
+                    res.json({"error": "an error occurred while trying to fetch tracks"});
+                    reject(e);
+                }
+            });
+            request(sptfyOptions, (e, r, b) => {
+                if (!e) {
+                    results = JSON.parse(b);
+                    tracks['spotify'] = results;
+                    resolve({ r, b });
+                } else {
+                    console.log(e);
+                    res.json({"error": "an error occurred while trying to fetch tracks"});
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    function mash(tracks) {
+        var mashed = []
+        soundcloud = tracks['soundcloud'];
+        spotify = tracks['spotify'];
+
+        soundcloud.forEach(function (track) {
+            m = {
+                'title': track.title,
+                'duration': track.duration,
+                'permlink': track.permalink_url,
+                'provider': 'SoundCloud'
+            };
+            mashed.push(m);
+        });
+        spotify.forEach(function (track) {
+            m = {
+                'title': track.name + ' - ' + track.artists[0].name,
+                'duration': track.duration_ms,
+                'permlink': track.external_urls.spotify,
+                'provider': 'Spotify'
+            };
+            mashed.push(m);
+        });
+
+        return mashed
+    }
+
+    var tracks = getTracks();
+    tracks.then(function(r) {
+        //tl = r;
+        var trackList = mash(tracks);
+        res.json(trackList);
+    }, function(e) {
+        console.log(e);
+        res.json({"error": "an error occurred while trying to fetch tracks"});
+    })
+})
+
+module.exports = router;
