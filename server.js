7d75d32d99ea3817ff15c9d0a6d92e315782140d
mortea15 <mortea15@student.uia.no>
2018-07-19 22:11:28 +0200

    enabled routes

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-//const SearchRoute = require('./routes/SearchRoute');
+const SearchRoute = require('./routes/SearchRoute');
 const SpotifyRoute = require('./routes/SpotifyRoute');
+const SoundcloudRoute = require('./routes/SoundCloudRoute');
 
 const PORT = 4000;
 
 app.use(bodyParser.json());
-//app.use('/search', SearchRoute);
-app.use('/spotify', SpotifyRoute)
+app.use('/search', SearchRoute);
+app.use('/spotify', SpotifyRoute);
+app.use('/soundcloud', SoundcloudRoute);
 
 app.listen(PORT, function() {
     console.log('musicmash-backend running on :', PORT);
