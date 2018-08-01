17c9aebc1d8ab3d767ea9d87283ae29a6d758082
mortea15 <mortea15@student.uia.no>
2018-08-01 20:49:26 +0200

    use sanitizer

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressSanitizer = require('express-sanitizer');
 const compression = require('compression');
 const helmet = require('helmet');
 
@@ -11,9 +12,12 @@ const SearchRoute = require('./routes/SearchRoute');
 const PORT = 5000;
 
 app.use(bodyParser.json());
-app.use('/search', SearchRoute);
+app.use(expressSanitizer());
 app.use(compression());
 app.use(helmet());
+
+app.use('/search', SearchRoute);
+
 app.listen(PORT, function() {
     console.log('musicmash-backend running on :', PORT);
 });
