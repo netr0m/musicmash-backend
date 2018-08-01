a579dd17bd7028daeaf57208ed92f1d23566af07
mortea15 <mortea15@student.uia.no>
2018-08-01 22:25:41 +0200

    fixed port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const helmet = require('helmet');
 const app = express();
 const SearchRoute = require('./routes/SearchRoute');
 
-const PORT = 5000;
+const PORT = 80;
 
 app.use(bodyParser.json());
 app.use(expressSanitizer());
