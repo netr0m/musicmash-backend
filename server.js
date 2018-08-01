96dea2832b185cf5b28ca7387ab2a8e38b63dc4c
mortea15 <mortea15@student.uia.no>
2018-08-01 19:39:40 +0200

    helmet and compression

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
 const SearchRoute = require('./routes/SearchRoute');
@@ -10,7 +12,8 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 app.use('/search', SearchRoute);
-
+app.use(compression());
+app.use(helmet());
 app.listen(PORT, function() {
     console.log('musicmash-backend running on :', PORT);
 });
