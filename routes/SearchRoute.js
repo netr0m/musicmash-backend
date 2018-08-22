7a3c60a8770bb52a5bfad95e42c5a10e7a93e403
mortea15 <mortea15@student.uia.no>
2018-08-22 14:38:43 +0200

    moved mash function to separate module

--- routes/SearchRoute.js
+++ routes/SearchRoute.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const router = express.Router();
 
+const mash = require('../helpers/mash');
+
 router.route('/:query').get(async (req, res) => {
     req.body.sanitized = req.sanitize(req.params.query);
     var q = req.body.sanitized.replace(/ /g, '%20');
-    var spotify_controller = require('./SpotifyController');
-    var soundcloud_controller = require('./SoundCloudController');
-    var youtube_controller = require('./YouTubeController');
+
+    const spotify_controller = require('./SpotifyController');
+    const soundcloud_controller = require('./SoundCloudController');
+    const youtube_controller = require('./YouTubeController');
 
     function getTracks() {
         return new Promise((resolve, reject) => {
@@ -21,61 +24,14 @@ router.route('/:query').get(async (req, res) => {
                 sc_tracks.then(function (sc_tracks) {
                     tracks['soundcloud'] = sc_tracks;
 
-		    var yt_tracks = youtube_controller.getTracks(q);
-		    yt_tracks.then(function (yt_tracks) {
-		    	tracks['youtube'] = yt_tracks;
-                    	resolve(tracks);
-		    });
+                    var yt_tracks = youtube_controller.getTracks(q);
+                    yt_tracks.then(function (yt_tracks) {
+                        tracks['youtube'] = yt_tracks;
+                        resolve(tracks);
+                    });
                 });
             });
         });
-    }
-
-    function mash(tracks) {
-        var mashed = {};
-        soundcloud = tracks['soundcloud'];
-        spotify = tracks['spotify'];
-	youtube = tracks['youtube'];
-
-        sptfy_parsed = [];
-        sc_parsed = [];
-	yt_parsed = [];
-
-        soundcloud.forEach(function (track) {
-            m = {
-                'title': track.title,
-                'duration': track.duration,
-                'permlink': track.permalink_url,
-                'provider': 'SoundCloud'
-            };
-            sc_parsed.push(m);
-        });
-        mashed['soundcloud'] = sc_parsed;
-        spotify.forEach(function (track) {
-            // TODO Add for-loop to add a few more artists if any
-            m = {
-                'title': track.name + ' - ' + track.artists[0].name,
-                'duration': track.duration_ms,
-                'permlink': track.external_urls.spotify,
-                'provider': 'Spotify'
-            };
-            sptfy_parsed.push(m);
-        });
-	mashed['spotify'] = sptfy_parsed;
-
-	yt_prefix='https://www.youtube.com/watch?v=';
-	youtube.forEach(function (track) {
-	    m = {
-                'title': track.snippet.title,
-                'duration': track.duration_ms,
-                'permlink': yt_prefix + track.id.videoId,
-                'provider': 'YouTube'
-            };
-            yt_parsed.push(m);
-        });
-	mashed['youtube'] = yt_parsed;
-
-        return mashed
     }
 
     var tracks = getTracks();
