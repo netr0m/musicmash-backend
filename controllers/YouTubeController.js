e377a02cda4846320438a50472f5823f5b028b65
mortea15 <mortea15@student.uia.no>
2018-10-14 00:15:21 +0200

    Rewritten to use axios instead of requests + separated logic

--- controllers/YouTubeController.js
+++ controllers/YouTubeController.js
@@ -1,42 +1,88 @@
-// SoundcloudController.js
+// YouTubeController.js
 
-const request = require('request')
+const axios = require('axios')
 const buildUrl = require('build-url')
 const moment = require('moment')
 
 const apiKey = process.env.YOUTUBE_KEY
 if (!apiKey) { console.log('Missing apiKey for Soundcloud') }
-const base = 'https://www.googleapis.com/youtube/v3'
+const baseUrl = 'https://www.googleapis.com/youtube/v3'
 
 function getTracks (query) {
-  return new Promise((resolve, reject) => {
-    const url = buildUrl(base, {
-      path: 'search',
-      queryParams: {
-        q: query,
-        type: 'video',
-        maxResults: 5,
-        part: 'snippet',
-        order: 'relevance',
-        key: apiKey
-      }
-    })
-
-    var options = {
-      url: url,
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      json: true
+  const url = buildUrl(baseUrl, {
+    path: 'search',
+    queryParams: {
+      q: query,
+      type: 'video',
+      maxResults: 5,
+      part: 'snippet',
+      order: 'relevance',
+      key: apiKey
     }
-    request(options, (e, r, b) => {
-      if (!e && r.statusCode === 200) {
-        var results = []
+  })
 
-        // Get the tracks
-        const trackList = b['items']
+  const config = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return axios.get(url, config)
+    .catch(function (err) {
+      if (err.response) {
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers)
+      } else if (err.request) {
+        console.log(err.request)
+      } else {
+        console.log('Error', err.message)
+      }
+      console.log(err.config)
+    })
+}
+
+function getContentDetails (trackIds) {
+  const url = buildUrl(baseUrl, {
+    path: 'videos',
+    queryParams: {
+      id: trackIds.toString(),
+      part: 'contentDetails',
+      key: apiKey
+    }
+  })
+
+  const config = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return axios.get(url, config)
+    .catch(function (err) {
+      if (err.response) {
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers)
+      } else if (err.request) {
+        console.log(err.request)
+      } else {
+        console.log('Error', err.message)
+      }
+      console.log(err.config)
+    })
+}
+
+function search (query) {
+  var results = []
+
+  return new Promise((resolve, reject) => {
+    const tracks = getTracks(query)
+    tracks
+      .then(function (tracksRes) {
+        const trackList = tracksRes.data.items
 
         var trackIds = []
         var details = {}
@@ -49,60 +95,33 @@ function getTracks (query) {
 
         // Get the contentDetails for the tracks
         const trackDetails = getContentDetails(trackIds)
-
-        trackDetails.then(function (trackDetails) {
-          trackDetails.forEach(function (trackDetail) {
-            var id = trackDetail.id
-            var duration = trackDetail.contentDetails.duration
-            details[id] = duration
+        trackDetails
+          .then(function (trackDetailsRes) {
+            const items = trackDetailsRes.data.items
+            items.forEach(function (trackDetail) {
+              const id = trackDetail.id
+              const duration = trackDetail.contentDetails.duration
+              details[id] = duration
+            })
+            trackList.forEach(function (result) {
+              const id = result.id.videoId
+              result.duration = moment.duration(details[id]).asMilliseconds()
+              results.push(result)
+            })
           })
-          trackList.forEach(function (result) {
-            var id = result.id.videoId
-            result.duration = moment.duration(details[id]).asMilliseconds()
-            results.push(result)
+          .then(function () {
+            resolve(results)
           })
-        }).then(function (tracks) {
-          resolve(results)
-        })
-      } else {
-        console.log(e)
-        reject(e)
-      }
-    })
+          .catch(function (err) {
+            console.log(err)
+            reject(err)
+          })
+      })
+      .catch(function (err) {
+        console.log(err)
+        reject(err)
+      })
   })
 }
 
-function getContentDetails (trackIds) {
-  return new Promise((resolve, reject) => {
-    const url = buildUrl(base, {
-      path: 'videos',
-      queryParams: {
-        id: trackIds.toString(),
-        part: 'contentDetails',
-        key: apiKey
-      }
-    })
-
-    var options = {
-      url: url,
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      json: true
-    }
-
-    request(options, (e, r, b) => {
-      if (!e && r.statusCode === 200) {
-        const results = b['items']
-        resolve(results)
-      } else {
-        console.log(e)
-        reject(e)
-      }
-    })
-  })
-}
-
-module.exports.getTracks = getTracks
+module.exports.search = search
