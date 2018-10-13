5a4757a13d6778cfa755429ab8f245c696d7f9df
mortea15 <mortea15@student.uia.no>
2018-10-13 17:59:01 +0200

    Promise-based API interaction with Spotify (using axios + Promise)

--- controllers/SpotifyController.js
+++ controllers/SpotifyController.js
@@ -4,15 +4,16 @@ const querystring = require('querystring')
 const buildUrl = require('build-url')
 
 /*
-Function to get an access token for authorization
+Function to get an access token for authentication
 */
 function getAccessToken () {
+  // Get credentials for Auth
   const clientID = process.env.SPOTIFY_ID
   if (!clientID) { console.log('Missing clientID for Spotify') }
   const clientSecret = process.env.SPOTIFY_SECRET
   if (!clientSecret) { console.log('Missing clientSecret for Spotify') }
 
-  const authBase = 'https://accounts.spotify.com/api/token'
+  const url = 'https://accounts.spotify.com/api/token'
 
   const authConfig = {
     headers: {
@@ -21,67 +22,58 @@ function getAccessToken () {
     }
   }
 
+  // Stringify using querystring (form)
   const body = querystring.stringify({
     grant_type: 'client_credentials'
   })
 
-  return axios.post(authBase, body, authConfig)
-    .catch(function (err) {
-      if (err.response) {
-        console.log(err.response.data)
-        console.log(err.response.status)
-        console.log(err.response.headers)
-      } else if (err.request) {
-        console.log(err.request)
-      } else {
-        console.log('Error', err.message)
-      }
-      console.log(err.config)
-    })
+  return axios.post(url, body, authConfig)
 }
 
 /*
 Function to get the tracks
 */
-function getTracks (query) {
+function getTracks (query, accessToken) {
   const base = 'https://api.spotify.com/v1'
 
-  // Get the access token
-  const getToken = getAccessToken()
-  getToken
-    .then(function (res) {
-      const accessToken = res.data.access_token
-      const config = {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + accessToken
-        }
-      }
+  const config = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken
+    }
+  }
 
-      const url = buildUrl(base, {
-        path: 'search',
-        queryParams: {
-          q: query,
-          type: 'track',
-          limit: 5
-        }
-      })
+  // Build the full URL
+  const url = buildUrl(base, {
+    path: 'search',
+    queryParams: {
+      q: query,
+      type: 'track',
+      limit: 5
+    }
+  })
 
-      return axios.get(url, config)
-        .catch(function (err) {
-          if (err.response) {
-            console.log(err.response.data)
-            console.log(err.response.status)
-            console.log(err.response.headers)
-          } else if (err.request) {
-            console.log(err.request)
-          } else {
-            console.log('Error', err.message)
-          }
-          console.log(err.config)
-        })
-    })
+  return axios.get(url, config)
 }
 
-module.exports.getTracks = getTracks
+/*
+Function that retrieves an access_token, then retrieves tracks
+*/
+function search (query) {
+  return new Promise((resolve, reject) => {
+    const auth = getAccessToken()
+    auth
+      .then(function (authRes) {
+        const accessToken = authRes.data.access_token
+
+        resolve(getTracks(query, accessToken))
+      })
+      .catch(function (err) {
+        console.log(err)
+        reject(err)
+      })
+  })
+}
+
+module.exports.search = search
