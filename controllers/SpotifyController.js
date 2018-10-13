7033f1b6d9fbf03dca7ce54d2b3de2145fcb4ed3
mortea15 <mortea15@student.uia.no>
2018-10-13 14:01:18 +0200

    Rewritten to use Axios, with built-in Promises, instead of requests wrapped in Promises

--- controllers/SpotifyController.js
+++ controllers/SpotifyController.js
@@ -1,68 +1,87 @@
 // SpotifyController.js
-
-const request = require('request')
+const axios = require('axios')
+const querystring = require('querystring')
 const buildUrl = require('build-url')
 
-const clientID = process.env.SPOTIFY_ID
-if (!clientID) { console.log('Missing clientID for Spotify') }
-const clientSecret = process.env.SPOTIFY_SECRET
-if (!clientSecret) { console.log('Missing clientSecret for Spotify') }
-const authBase = 'https://accounts.spotify.com/api/token'
-const base = 'https://api.spotify.com/v1'
+/*
+Function to get an access token for authorization
+*/
+function getAccessToken () {
+  const clientID = process.env.SPOTIFY_ID
+  if (!clientID) { console.log('Missing clientID for Spotify') }
+  const clientSecret = process.env.SPOTIFY_SECRET
+  if (!clientSecret) { console.log('Missing clientSecret for Spotify') }
 
-function getTracks (query) {
-  return new Promise((resolve, reject) => {
-    var results = []
-    // Authorization
-    var authOptions = {
-      url: authBase,
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
-      },
-      form: {
-        grant_type: 'client_credentials'
-      },
-      json: true
+  const authBase = 'https://accounts.spotify.com/api/token'
+
+  const authConfig = {
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64')),
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
-    request.post(authOptions, (e, r, b) => {
-      if (!e && r.statusCode === 200) {
-        var token = b.access_token
+  }
 
-        const url = buildUrl(base, {
-          path: 'search',
-          queryParams: {
-            q: query,
-            type: 'track',
-            limit: 5
-          }
-        })
-
-        var options = {
-          url: url,
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-          },
-          json: true
-        }
-        request(options, (e, r, b) => {
-          if (!e && r.statusCode === 200) {
-            const tracklist = b['tracks']
-            results = tracklist['items']
-            resolve(results)
-          } else {
-            console.log(e)
-            reject(e)
-          }
-        })
-      } else {
-        console.log(e)
-        reject(e)
-      }
-    })
+  const body = querystring.stringify({
+    grant_type: 'client_credentials'
   })
+
+  return axios.post(authBase, body, authConfig)
+    .catch(function (err) {
+      if (err.response) {
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers)
+      } else if (err.request) {
+        console.log(err.request)
+      } else {
+        console.log('Error', err.message)
+      }
+      console.log(err.config)
+    })
+}
+
+/*
+Function to get the tracks
+*/
+function getTracks (query) {
+  const base = 'https://api.spotify.com/v1'
+
+  // Get the access token
+  const getToken = getAccessToken()
+  getToken
+    .then(function (res) {
+      const accessToken = res.data.access_token
+      const config = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + accessToken
+        }
+      }
+
+      const url = buildUrl(base, {
+        path: 'search',
+        queryParams: {
+          q: query,
+          type: 'track',
+          limit: 5
+        }
+      })
+
+      return axios.get(url, config)
+        .catch(function (err) {
+          if (err.response) {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+          } else if (err.request) {
+            console.log(err.request)
+          } else {
+            console.log('Error', err.message)
+          }
+          console.log(err.config)
+        })
+    })
 }
 
 module.exports.getTracks = getTracks
