588fc8fbad72f95198e8af8515e96187a24287ce
mortea15 <mortea15@student.uia.no>
2018-10-15 11:58:10 +0200

    Added parameter to define number of results to return (limit)

--- controllers/search.js
+++ controllers/search.js
@@ -5,12 +5,12 @@ const spotifyController = require('./SpotifyController')
 const soundcloudController = require('./SoundCloudController')
 const youtubeController = require('./YouTubeController')
 
-function getTracks (query) {
+function getTracks (query, limit) {
   var tracks = {}
 
   return new Promise((resolve, reject) => {
     // Execute the functions concurrently
-    axios.all([spotifyController.search(query), soundcloudController.search(query), youtubeController.search(query)])
+    axios.all([spotifyController.search(query, limit), soundcloudController.search(query, limit), youtubeController.search(query, limit)])
       .then(axios.spread(function (spotifyResult, soundcloudResult, youtubeResult) {
         tracks['spotify'] = spotifyResult.data.tracks.items
         tracks['soundcloud'] = soundcloudResult.data.collection
@@ -35,10 +35,13 @@ function getTracks (query) {
 
 const api = {
   search: (req, res) => {
-    req.body.sanitized = req.sanitize(req.query.q)
-    const q = req.body.sanitized.replace(/ /g, '+')
+    req.body.sanitized = {}
+    req.body.sanitized.q = req.sanitize(req.query.q)
+    req.body.sanitized.limit = req.sanitize(req.query.limit)
+    const q = req.body.sanitized.q.replace(/ /g, '+')
+    const l = req.body.sanitized.limit || 5
 
-    const tracks = getTracks(q)
+    const tracks = getTracks(q, l)
 
     tracks
       .then(function (tracks) {
