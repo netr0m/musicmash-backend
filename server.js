06e8a18f85a2a433f414fceff7e6705fd706cae8
mortea15 <mortea15@student.uia.no>
2018-10-15 11:29:06 +0200

    refactored to use new route handling structure

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ const bodyParser = require('body-parser')
 const expressSanitizer = require('express-sanitizer')
 const compression = require('compression')
 const helmet = require('helmet')
+const logger = require('morgan')
+
+const routes = require('./routes/index')
 
 const app = express()
-const SearchRoute = require('./routes/SearchRoute')
+const router = express.Router()
 
-const PORT = 8080
+const environment = process.env.NODE_ENV
+const stage = require('./config')[environment]
+const PORT = stage.port
+
+if (environment !== 'production') {
+  app.use(logger('dev'))
+}
 
 app.use(bodyParser.json())
 app.use(expressSanitizer())
 app.use(compression())
 app.use(helmet())
 
-app.use('/search', SearchRoute)
+app.use('/api/v1', routes(router))
 
 app.listen(PORT, function () {
   console.log('musicmash-backend running on :', PORT)
